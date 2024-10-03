a593d5298ff7f0070ec26555f70c12101088f6b1
Sansar Tiwari <st9889477@gmail.com>
2024-10-03 21:31:47 +0530

    "Added environment variables for access and refresh tokens, and implemented password hashing, token generation, and comparison in user model"

--- WebProject/src/models/user.models.js
+++ WebProject/src/models/user.models.js
@@ -49,6 +49,39 @@ const userSchema = new Schema({
   timestamps:true
 });
 
-userSchema.pre('save')
+userSchema.pre('save',async function(next) {
+  if(!this.isModified("password")) return  next()
+  this.password = bcrypt.hash(this.password,10)
+  next()
+})
 
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+return jwt.sign(
+    {
+       _id:this._id,
+       email:this.email,
+       username:this.username,
+       fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECERT,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+userSchema.methods.genarateRefreshToken = function(){
+  return jwt.sign(
+    {
+       _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECERT,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
 export const User = mongoose.model("User", userSchema);
