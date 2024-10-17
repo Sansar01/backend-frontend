ca71d085f539d67cbe0687138249baf7e265a47e
Sansar Tiwari <st9889477@gmail.com>
2024-10-17 21:47:37 +0530

    "Added cookie-parser dependency and implemented cookie handling in index.js"

--- WebappwithBackend/index.js
+++ WebappwithBackend/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const cookieParser = require('cookie-parser')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser())
 
 app.set("view engine", "ejs");
 
@@ -13,28 +15,41 @@ app.set("view engine", "ejs");
 //     res.render("index")
 // })
 
-app.get("/", function (req, res) {
-  fs.readdir("./files", function (err, files) {
-    res.render("index", { files: files });
-  });
-});
 
-app.post("/create", function (req, res) {
-  fs.writeFile(
-    `./files/${req.body.title.split("").join("")}.txt`,
-    req.body.detail,
-    function (err) {
-      console.log("errro ",err); 
-    }
-  );
-  res.redirect('/')
-});
-
-app.get('/files/:filename',function(req,res){
-  fs.readFile(`./files/${req.params.filename}.txt`, 'utf8', function(err,filedata){
-    res.render('show',{filename:req.params.filename,filedata:filedata})
-  })
+app.get('/',function(req,res){
+  res.cookie('name','Sansar')
+  res.send("Hello,Welcome ");
 })
+
+app.get('/cookie',function(req,res){
+  res.send("This is cookie data store on your browser")
+  console.log(req.cookies);
+  
+})
+
+// app.get("/", function (req, res) {
+//   fs.readdir("./files", function (err, files) {
+//     res.render("index", { files: files });
+//   });
+// });
+
+// app.post("/create", function (req, res) {
+//   fs.writeFile(
+//     `./files/${req.body.title.split("").join("")}.txt`,
+//     req.body.detail,
+//     function (err) {
+//       console.log("errro ",err); 
+//     }
+//   );
+//   res.redirect('/')
+// });
+
+// app.get('/files/:filename',function(req,res){
+//   fs.readFile(`./files/${req.params.filename}.txt`, 'utf8', function(err,filedata){
+//     res.render('show',{filename:req.params.filename,filedata:filedata})
+//   })
+// })
+
 app.listen(5000, () => {
   console.log("Server is running on port : 5000");
 });
